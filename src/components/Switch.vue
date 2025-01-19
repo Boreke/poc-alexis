077c11a65ed4ec07482ba3ced4610ac458c9b3d5
Boreke <hippomartin03@gmail.com>
2025-01-19 01:24:16 +0100

    ajout switch sur header

---
+++ src/components/Switch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Switch
+      v-model="enabled"
+      :class="enabled ? 'bg-blue-900' : 'bg-blue-600'"
+      class="relative inline-flex h-[24px] w-[40px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+    >
+      <span
+        aria-hidden="true"
+        :class="enabled ? 'translate-x-4' : 'translate-x-0'"
+        class="pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white ring-0 transition duration-200 ease-in-out"
+      />
+    </Switch>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Switch } from '@headlessui/vue'
+
+const enabled = ref(false)
+</script>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,22 +2,31 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-		colors: {
-			primary: {
-				100: "#d3d3d3",
-				200: "#a6a6a6",
-				300: "#7a7a7a",
-				400: "#4d4d4d",
-				500: "#212121",
-				600: "#1a1a1a",
-				700: "#141414",
-				800: "#0d0d0d",
-				900: "#070707"
-			},
-			'white': '#ffffff',
-			'transparent': 'transparent',
-			
+		extend: {
+			colors: {
+				'primary': {
+					100: "#d3d3d3",
+					200: "#a6a6a6",
+					300: "#7a7a7a",
+					400: "#4d4d4d",
+					500: "#212121",
+					600: "#1a1a1a",
+					700: "#141414",
+					800: "#0d0d0d",
+					900: "#070707"
+				},
+				'secondary': {
+					100: "#d3d3d3",
+					200: "#a6a6a6",
+					300: "#7a7a7a",
+					400: "#4d4d4d",
+					500: "#212121",
+					600: "#1a1a1a",
+					700: "#141414",
+					800: "#0d0d0d",
+					900: "#070707"
+				},
+			}
 		},
 	},
 	plugins: [],
